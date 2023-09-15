ede1c223a2e1e6f34213726d515321d551f02433
Diego Sano <diegocsano@outlook.com>
2023-09-15 10:28:33 -0300

    feat: route to upload video

--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -1,4 +1,5 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
+import { utapi } from 'uploadthing/server'
 
 import prisma from '@/lib/db'
 
@@ -8,6 +9,54 @@ export async function GET() {
 
     return NextResponse.json({ videos })
   } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    )
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData()
+    const file: File | null = formData.get('file') as File
+
+    if (!file) {
+      return NextResponse.json(
+        {
+          error: 'Missing file input.',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    const response = await utapi.uploadFiles(file)
+
+    if (response.error) {
+      return NextResponse.json(
+        {
+          error: response.error.message,
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    const video = await prisma.video.create({
+      data: {
+        name: response.data.name,
+        url: response.data.url,
+      },
+    })
+
+    return NextResponse.json({ video })
+  } catch (error) {
+    console.log({
+      error,
+    })
     return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: 500 },
